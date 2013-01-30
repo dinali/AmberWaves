60db830976abdd7935e6cfbd05fb07c073cd3a49
Dina Li <dinali@ers.usda.gov>
2013-01-30 14:44:52 -0500

    Initial Commit

---
+++ AmberWavesTests/AmberWavesTests.h
@@ -0,0 +1,13 @@
+//
+//  AmberWavesTests.h
+//  AmberWavesTests
+//
+//  Created by Dina Li on 1/30/13.
+//  Copyright (c) 2013 USDA ERS. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface AmberWavesTests : SenTestCase
+
+@end
---
+++ AmberWavesTests/AmberWavesTests.m
@@ -0,0 +1,32 @@
+//
+//  AmberWavesTests.m
+//  AmberWavesTests
+//
+//  Created by Dina Li on 1/30/13.
+//  Copyright (c) 2013 USDA ERS. All rights reserved.
+//
+
+#import "AmberWavesTests.h"
+
+@implementation AmberWavesTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in AmberWavesTests");
+}
+
+@end
